56535069226581124e52c22954a0180b97f69516
Azzam <azzamalimc2@gmail.com>
2024-02-06 15:00:22 -0800

    set up the project

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from flask_talisman import Talisman
+from post_factory import PostFactory
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """Root URL response"""
+    app.logger.info("Request for Root URL")
+    return (jsonify(name="Data Retrieval Service", version="1.0"), 200)
+
+
+@app.route("/posts", methods=["GET"])
+def get_data():
+    """Returns forum post data"""
+    data = PostFactory.generate(5)
+    return jsonify(data)
